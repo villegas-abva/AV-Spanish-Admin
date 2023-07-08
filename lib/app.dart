bd42b0f1abf8303799239b03b6854c13420508f9
Andrew Villegas <andrew.villegas@bottlerocketstudios.com>
2023-07-07 22:15:33 -0500

    Cleanup: set AddTFQuizScreen as default screen

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:av_spanish_admin/services/repository/quiz_repository.dart';
 import 'package:av_spanish_admin/tf/add_tf_quiz/add_tf_quiz_screen.dart';
 import 'package:av_spanish_admin/tf/add_tf_quiz_questions_state_notifier.dart';
 import 'package:av_spanish_admin/tf/bloc/tf_quiz/individual_tf_quiz_bloc.dart';
-import 'package:av_spanish_admin/tf/edit_tf_quiz/edit_tf_quiz_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
@@ -20,13 +19,14 @@ class _AvSpanishAppState extends State<AVSpanishAdminApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ChangeNotifierProvider(
-          create: (context) => AddTFQuizQuestionsStateNotifier(),
-          // child: BlocProvider(
-          //   create: (context) =>
-          //       IndividualTFQuizBloc(quizRepository: QuizRepository()),
-          //   child: const AddTFQuizScreen(),
-          // ),
-          child: const EditTFQuizScreen()),
+        create: (context) => AddTFQuizQuestionsStateNotifier(),
+        child: BlocProvider(
+          create: (context) =>
+              IndividualTFQuizBloc(quizRepository: QuizRepository()),
+          child: const AddTFQuizScreen(),
+        ),
+      ),
+      // child: const EditTFQuizScreen()),
     );
   }
 }
