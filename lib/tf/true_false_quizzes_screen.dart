b612739c90355d27d7ec2141b9dccfdb7da03f94
Andrew Villegas <andrew.villegas@bottlerocketstudios.com>
2023-08-05 21:46:59 -0500

    Add tf quizzes screen && widgets

--- lib/tf/true_false_quizzes_screen.dart
+++ lib/tf/true_false_quizzes_screen.dart
@@ -1,9 +1,17 @@
+import 'package:av_spanish_admin/services/models/tf/tf_quiz.dart';
 import 'package:av_spanish_admin/tf/widgets/tf_quiz_screen_app_bar.dart';
+import 'package:av_spanish_admin/tf/widgets/tf_quizzes_list_widget.dart';
 import 'package:av_spanish_admin/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class TrueFalseQuizzesScreen extends StatelessWidget {
-  const TrueFalseQuizzesScreen({super.key});
+  /// Displays a list of [TFQuiz]s.
+  const TrueFalseQuizzesScreen({
+    required this.quizzes,
+    super.key,
+  });
+
+  final List<TFQuiz> quizzes;
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +27,14 @@ class TrueFalseQuizzesScreen extends StatelessWidget {
                 padding: EdgeInsets.only(top: 60, bottom: 20),
                 child: TFQuizScreenAppBar(),
               ),
-              AppText(
-                'True False Quizzes',
-                style: AppText.headline1Style.copyWith(fontSize: 28),
-              )
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: AppText(
+                  'True False Quizzes',
+                  style: AppText.headline1Style.copyWith(fontSize: 28),
+                ),
+              ),
+              TFQuizzesListWidget(quizzes: quizzes),
             ],
           ),
         ),
